beb49d71be6826ca64fbe8517f925d60d11eeb15
SHASHANK-2004-jais <sj440848@gmail.com>
2026-04-07 23:17:17 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,17 @@
+student_name = "Shanky"
+student_age = 21
+student_marks = 89.5
+passed = True
+
+
+print("Name:", student_name)
+print("Age:", student_age)
+print("Marks:", student_marks)
+print("Passed:", passed)
+
+
+print("Data Types:")
+print(type(student_name))
+print(type(student_age))
+print(type(student_marks))
+print(type(passed))
