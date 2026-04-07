29550359c2b642521581aaec86a8fdd4e3f1e15e
SHASHANK-2004-jais <sj440848@gmail.com>
2026-04-07 23:32:19 +0530

    Add files via upload

---
+++ string_method.py
@@ -0,0 +1,16 @@
+
+text = input("Enter a string: ")
+
+new_text = text + " Python"
+
+print("Concatenated String:", new_text)
+
+print("First character:", text[0])
+
+print("First 3 characters:", text[:3])
+
+print("Upper case:", text.upper())
+print("Lower case:", text.lower())
+print("Split:", text.split())
+print("Replace:", text.replace("a", "@"))
+print("Length:", len(text))
