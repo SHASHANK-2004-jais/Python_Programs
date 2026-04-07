51e8311c08b0d6e3f043190edcc828ef9087e9a4
SHASHANK-2004-jais <sj440848@gmail.com>
2026-04-07 23:20:42 +0530

    Add files via upload

---
+++ convert_type.py
@@ -0,0 +1,19 @@
+
+num = 10
+
+
+float_num = float(num)
+
+
+decimal_num = 15.75
+int_num = int(decimal_num)
+
+
+string_num = str(num)
+
+
+print("Original Integer:", num, type(num))
+print("Converted to Float:", float_num, type(float_num))
+print("Original Float:", decimal_num, type(decimal_num))
+print("Converted to Integer:", int_num, type(int_num))
+print("Converted to String:", string_num, type(string_num))
